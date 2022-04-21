8d88f33fd62ea34509af351e7a19d636d3d14f47
Sean Murphy <sdmurphy.1999@gmail.com>
2022-04-21 14:32:53 -0400

    Adds image views with buttons and connects to VC

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPushed(_ sender: UIButton) {
+    }
+    
 }
 
