ab67952330d16fd425e0eeabc9e56ad2aa3894a3
Sean Murphy <sdmurphy.1999@gmail.com>
2022-04-21 18:14:09 -0400

    Adds dice randomization

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,12 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImage1: UIImageView!
     @IBOutlet weak var diceImage2: UIImageView!
     
+    let imageArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func rollButtonPushed(_ sender: UIButton) {
+        diceImage1.image = imageArray.randomElement() as! UIImage
+        diceImage2.image = imageArray.randomElement() as! UIImage
     }
     
 }
